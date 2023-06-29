bd73fac5c5f306cc0a882e1b8e7b63b968df005a
limjuhee0803 <limjuhee0803@naver.com>
2023-06-29 17:57:37 +0900

    포트폴리오 파일 수정

--- portfolio-main/main_portfolio/pc_script.js
+++ portfolio-main/main_portfolio/pc_script.js
@@ -23,3 +23,55 @@ $(window).on('scroll', (e) => {
     $('.back_top img ').removeClass('change_daisy');
   }
 });
+
+// UX design
+let currnetIdx = 0;
+
+$('.slideBtn li').on('click', (e) => {
+  const target = $(e.target);
+  const idx = target.index();
+
+  $('.slideWrapper')
+    .stop()
+    .animate(
+      {
+        marginLeft: -100 * idx + '%',
+      },
+      700
+    );
+
+  $('.slideBtn li').removeClass('active');
+  target.addClass('active');
+
+  currnetIdx = idx; // 현재 인덱스에 내가 클릭한 인덱스 번호 대입
+});
+
+const autoSlide = () => {
+  currnetIdx = (currnetIdx + 1) % 4; // 0,1,2,3
+
+  $('.slideWrapper')
+    .stop()
+    .animate(
+      {
+        marginLeft: -100 * currnetIdx + '%',
+      },
+      700
+    );
+
+  $('.slideBtn li')
+    .eq(currnetIdx)
+    .addClass('active')
+    .siblings()
+    .removeClass('active');
+};
+
+let mainSlide = setInterval(autoSlide, 3000);
+
+$('.simpleSlide').hover(
+  function () {
+    clearInterval(mainSlide);
+  },
+  function () {
+    mainSlide = setInterval(autoSlide, 3000);
+  }
+);
