04e3e9f2ed84ab571680a3073b4049bbe63a5ea8
limjuhee0803 <limjuhee0803@naver.com>
2023-07-16 12:35:16 +0900

    임주희 jeep 파일 수정

--- project/jeep/jeep.js
+++ project/jeep/jeep.js
@@ -17,3 +17,47 @@ const mySwiper1 = new Swiper('.mySwiper1', {
       delay: 4000,
     },
   });
+
+  // best model 
+  $('.best-model-menu .btn1').on('click', function () {
+    let idx = $(this).index();
+    $('.best-model-img img').attr({
+      src: `./images/bestmodel1.png`,
+      alt: 'bestmodel',
+    })
+  });
+
+  $('.best-model-menu .btn2').on('click', function () {
+    let idx = $(this).index();
+    $('.best-model-img img').attr({
+      src: `./images/bestmodel2.png`,
+      alt: 'bestmodel2',
+    })
+  });
+
+  $('.best-model-menu .btn3').on('click', function () {
+    let idx = $(this).index();
+    $('.best-model-img img').attr({
+      src: `./images/bestmodel3.png`,
+      alt: 'bestmodel3',
+    })
+  });
+
+
+  $('.best-model-menu .btn1').on('click', () => {
+    $('.best-model-menu .btn1').addClass('active')
+    $('.best-model-menu .btn2').removeClass('active')
+    $('.best-model-menu .btn3').removeClass('active')
+  });
+
+  $('.best-model-menu .btn2').on('click', () => {
+    $('.best-model-menu .btn2').addClass('active')
+    $('.best-model-menu .btn1').removeClass('active')
+    $('.best-model-menu .btn3').removeClass('active')
+  });
+
+  $('.best-model-menu .btn3').on('click', () => {
+    $('.best-model-menu .btn3').addClass('active')
+    $('.best-model-menu .btn1').removeClass('active')
+    $('.best-model-menu .btn2').removeClass('active')
+  });
