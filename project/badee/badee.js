c8ac59b2a397a064d7ef9b7a8ef3c62fb3c5f9e9
limjuhee0803 <limjuhee0803@naver.com>
2023-07-20 10:59:51 +0900

    임주희 badee 수정

--- project/badee/badee.js
+++ project/badee/badee.js
@@ -28,3 +28,21 @@ $('.up').on('click', () => {
     behavior: 'smooth',
   });
 });
+
+// banner 마우스 이벤트
+
+const img1 = document.querySelector('.item1');
+const img2 = document.querySelector('.item2');
+const img3 = document.querySelector('.item3');
+const img4 = document.querySelector('.item4');
+
+addEventListener('mousemove', (evt) => {
+  let x = evt.clientX,
+    y = evt.clientY;
+  console.log(x, y);
+
+  img1.style.transform = `translate(${x / 60}px,${-y / 50}px)`;
+  img2.style.transform = `translate(${x / -50}px,${-y / 100 + 10}px)`;
+  img3.style.transform = `translate(${30 - x / 60}px,${-y / 50}px)`;
+  img4.style.transform = `translate(${50 - x / 80}px,${-y / 20}px)`;
+});
