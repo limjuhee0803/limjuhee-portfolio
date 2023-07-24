8a27fa9a2076b6f600bba272821b737a4197a840
limjuhee0803 <limjuhee0803@naver.com>
2023-07-24 11:09:13 +0900

    임주희 수정

--- project/badee/badee-detail.js
+++ project/badee/badee-detail.js
@@ -1,8 +1,8 @@
 //menu
 $('.hambuger').on('click', () => {
-    $('.menu').toggleClass('change-menu');
-    $('.hambuger').toggleClass('change-hambuger');
-  });
+  $('.menu').toggleClass('change-menu');
+  $('.hambuger').toggleClass('change-hambuger');
+});
 
 //header
 let prevScroll = window.scrollY;
@@ -20,20 +20,52 @@ $(window).on('scroll', () => {
   console.log('prevScroll:', prevScroll);
 });
 
-
 // 내 포트폴리오 바로가기
 $('.footer-butterfly').hover(
-    (e) => {
-      $('.footer-butterfly').addClass('active-portfolio');
-    },
-    (e) => {
-      $('.footer-butterfly').removeClass('active-portfolio');
-    }
+  (e) => {
+    $('.footer-butterfly').addClass('active-portfolio');
+  },
+  (e) => {
+    $('.footer-butterfly').removeClass('active-portfolio');
+  }
+);
+
+$('.footer-butterfly.active-portfolio').on('click', () => {
+  $('.footer-butterfly.active-portfolio').attr(
+    'href',
+    'https://limjuhee0803.github.io/limjuhee-portfolio/index.html'
   );
-  
-  $('.footer-butterfly.active-portfolio').on('click', () => {
-    $('.footer-butterfly.active-portfolio').attr(
-      'href',
-      'https://limjuhee0803.github.io/limjuhee-portfolio/index.html'
-    );
+});
+// size 버튼 클릭
+$('.main-rt .size > p').on('click', () => {
+  $('.main-rt .size > p').toggleClass('active-size');
+});
+
+// buy 버튼 클릭
+$('.buy-box li')
+  .eq(1)
+  .on('click', () => {
+    $('.buy-box li').eq(1).toggleClass('active-box');
   });
+
+$('.buy-box li')
+  .eq(2)
+  .on('click', () => {
+    $('.buy-box li').eq(2).toggleClass('active-box');
+  });
+
+//hover 시 메인 이미지 변경
+$('.main-lf img').hover(
+  (e) => {
+    $('.main-lf img').attr({
+      src: `./images/detail-main2.png`,
+      alt: 'detail-main2',
+    });
+  },
+  (e) => {
+    $('.main-lf img').attr({
+      src: `./images/detail-main1.png`,
+      alt: 'detail-main1',
+    });
+  }
+);
